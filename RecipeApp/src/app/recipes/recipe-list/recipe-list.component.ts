6dd2f57ea6a181d68e08675abf1ee40401ca2f67
Steve Wall <syntakks@gmail.com>
2020-03-21 20:07:00 -0500

    Lots o stuff

--- RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Name',
+      'This is a test description... 1',
+      'https://media1.s-nbcnews.com/i/newscms/2018_33/1361101/omelet-eggs-mushroom-breakfast-stock-recipe-today-180817_18f5fc8ad9319ab76064b65e28429964.jpg'
+    ),
+    new Recipe(
+      'Test Name',
+      'This is a test description... 2',
+      'https://media1.s-nbcnews.com/i/newscms/2018_33/1361101/omelet-eggs-mushroom-breakfast-stock-recipe-today-180817_18f5fc8ad9319ab76064b65e28429964.jpg'
+    )
+  ];
 
   constructor() { }
 
--- RecipeApp/src/app/shopping-list/shopping-list.component.ts
+++ RecipeApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Orange', 5),
+    new Ingredient('Apple', 2),
+    new Ingredient('Tomato', 10)
+  ];
 
   constructor() { }
 
